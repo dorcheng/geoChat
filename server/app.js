11ad7940e5d113ae378b3862289f156aa2cb92ee
Doris Cheng <dorcheng@umich.edu>
2018-04-15 11:54:04 -0400

    changed port to make it compatible with heroku

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use((err, req, res, next) => {
   res.send('error: ' + err);
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log('The server is listening on port', port);
 });
