a43892cef09e11218b504e6b63f98dae772c3f91
Doris Cheng <dorcheng@umich.edu>
2018-04-14 23:12:21 -0400

    able to display real time messages

--- server/app.js
+++ server/app.js
@@ -1,42 +1,49 @@
-var express = require('express');
-var volleyball = require('volleyball');
-var bodyParser = require('body-parser');
-var path = require('path');
+const express = require('express');
+const volleyball = require('volleyball');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.use(volleyball);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   console.error(err, err.stack);
   res.status(err.status || 500);
   res.send('error: ' + err);
 });
 
-var port = 3000;
-var server = app.listen(port, function() {
-  console.log('The server is listening closely on port', port);
+const port = 3000;
+const server = app.listen(port, () => {
+  console.log('The server is listening on port', port);
 });
 
-var socketio = require('socket.io');
+//server that mounts on NodeJS HTTP server
+const socketio = require('socket.io');
 
-var io = socketio(server);
+const io = socketio(server);
 
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
   console.log(`User ${socket.id} has joined the map`);
   socket.emit('new-user', socket.id);
 
-  socket.on('disconnect', function(){
+  socket.on('chatMessage', (msg) => {
+    console.log('New Message:', msg);
+    socket.emit('new-message', msg);
+  });
+
+  socket.on('disconnect', () => {
     console.log(`User ${socket.id} has left the map`);
   });
 });
